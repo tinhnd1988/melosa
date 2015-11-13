16977a1322f17503086620af02f3df6ce1c2ac78
An <an.ho1991@gmail.com>
2015-11-14 02:11:46 +0700

    Update new design/Make CALL US to Dynamic

--- wp-content/themes/melosa/about.php
+++ wp-content/themes/melosa/about.php
@@ -9,40 +9,16 @@ get_header();
 <div class="pageHeader">
 				<div class="call_us">
 					<div class="icon_call"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/phone.png"></div>
-					<ul>
-						<li>
-							<div class="title_call">CEN Group</div>
-							<span>0903.85.99.11</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPBĐS La Bàn</div>
-							<span>0912 961 727</span>
-						</li>
-						<li>
-							<div class="title_call">Sàn GD SunLand SG</div>
-							<span>0938 779 660</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPTV TCF Việt Nam</div>
-							<span>0908 49 6446</span>
-						</li>
-						<li>
-							<div class="title_call">CTY TNHH Hưng Thịnh Phú</div>
-							<span>0934 839 868</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPBĐS TECHCOMREAL</div>
-							<span>0908 06 06 79</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPTV Đông Nam Á</div>
-							<span>0909 333 447</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPMG BĐS VÂN HÀ</div>
-							<span>0906 684 499</span>
-						</li>
-					</ul>
+					<?php
+						$args = array(
+						  'name'        => 'call-us',
+						  'post_type'   => 'post',
+						  'post_status' => 'publish',
+						  'numberposts' => 1
+						);
+						$callus = get_posts($args);
+						echo $callus[0]->post_content;
+					?>
 				</div>				
 				<div class="titleBar">
 				<?php
@@ -53,12 +29,13 @@ get_header();
 						);
 					$posts = get_posts( $args );
 				?>
-					<h1>Giới thiệu</h1>
-					<ul>
-						<?php foreach ($posts as $key => $item) :?>
-							<li class="ui_tabs <?php echo ($item->post_name == 'chu-dau-tu') ? 'active': ''?>"><span class="tab<?php echo $key ?>"><?php echo $item->post_title?></span></li>
-						<?php endforeach;?>
-					</ul>
+					<div class="container">
+						<ul>
+							<?php foreach ($posts as $key => $item) :?>
+								<li class="ui_tabs <?php echo ($item->post_name == 'chu-dau-tu') ? 'active': ''?>"><span class="tab<?php echo $key ?>"><?php echo $item->post_title?></span></li>
+							<?php endforeach;?>
+						</ul>
+					</div>
 				</div>
 			</div>
 			<div class="post_content">
