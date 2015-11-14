dc51b718352d73b65f768568e94e34a4f4b00753
githubminh <minh@wiserobot.com>
2015-11-14 16:49:57 +0700

    update

--- wp-content/themes/melosa/contact.php
+++ wp-content/themes/melosa/contact.php
@@ -91,9 +91,5 @@ get_header();
 					</div>
 				</div>
 			</div>
-					<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-			<div id="widget-area" class="widget-area" role="complementary">
-				<?php dynamic_sidebar( 'sidebar-1' ); ?>
-			</div><!-- .widget-area -->
-		<?php endif; ?>
+					
 <?php get_footer(); ?>
