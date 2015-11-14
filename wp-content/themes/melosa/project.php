6677cd93f5b381446f7153ea38680836b0c175f9
An <an.ho1991@gmail.com>
2015-11-14 17:24:58 +0700

    PROJECT - Change to Dynamic content

--- wp-content/themes/melosa/project.php
+++ wp-content/themes/melosa/project.php
@@ -8,53 +8,65 @@ get_header();
 ?>
 <div class="pageHeader">
 				<div class="call_us">
-					<div class="icon_call"><img src="images/phone.png"></div>
-					<ul>
-						<li>
-							<div class="title_call">CEN Group</div>
-							<span>0903.85.99.11</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPBĐS La Bàn</div>
-							<span>0912 961 727</span>
-						</li>
-						<li>
-							<div class="title_call">Sàn GD SunLand SG</div>
-							<span>0938 779 660</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPTV TCF Việt Nam</div>
-							<span>0908 49 6446</span>
-						</li>
-						<li>
-							<div class="title_call">CTY TNHH Hưng Thịnh Phú</div>
-							<span>0934 839 868</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPBĐS TECHCOMREAL</div>
-							<span>0908 06 06 79</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPTV Đông Nam Á</div>
-							<span>0909 333 447</span>
-						</li>
-						<li>
-							<div class="title_call">CTY CPMG BĐS VÂN HÀ</div>
-							<span>0906 684 499</span>
-						</li>
-					</ul>
-				</div>				
+					<div class="icon_call"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/phone.png"></div>
+					<?php
+						$args = array(
+						  'name'        => 'call-us',
+						  'post_type'   => 'post',
+						  'post_status' => 'publish',
+						  'numberposts' => 1
+						);
+						$callus = get_posts($args);
+						echo $callus[0]->post_content;
+					?>
+				</div>			
 				<div class="titleBar">
+				<?php
+					$parentCate = get_category_by_slug( 'du-an' );
+					$args = array(
+						'type'                     => 'post',
+						'child_of'                 => $parentCate->term_id,
+						'orderby'                  => 'name',
+						'order'                    => 'ASC',
+						'hide_empty'               => FALSE,
+						'hierarchical'             => 1,
+						'taxonomy'                 => 'category',
+					); 
+					$childCates = get_categories($args );
+					// var_dump($childCates);
+				?>
 					<div class="container">
 						<ul>
-							<li class="ui_tabs active"><span class="Tab1">Biệt thự </span></li>
-							<li class="ui_tabs"><span class="Tab2">Liên kết vườn</span></li>
-							<li class="ui_tabs"><span class="Tab3">Mặt bằng tổng thể</span></li>
+							<?php foreach ($childCates as $key => $item) :?>
+								<li class="ui_tabs <?php echo ($item->slug == 'biet-thu') ? 'active': ''?>"><span class="tab<?php echo $key ?>"><?php echo $item->name?></span></li>
+							<?php endforeach;?>
 						</ul>
 					</div>
 				</div>
 			</div>
 			<div class="post_content">
+				<?php
+					foreach ($childCates as $key => $cate) :
+						$args = array(
+							'category_name' => $cate->slug,
+							'orderby'       => 'date',
+							'order'         => 'ASC',
+							'post_type'     => 'post',
+							);
+						$posts = get_posts( $args );
+				?>
+							<div id="tab<?php echo $key ?>" class="content_tab" style="display:none;">
+								<div class="wrap-slide">
+									<?php 
+										foreach ($posts as $key => $item) :
+											echo apply_filters('the_content',$item->post_content); 
+										endforeach;		
+									?>
+								</div>
+							</div>					
+				<?php
+					endforeach;
+				?>
 				<div class="projectContent">
 					<div id="Tab1" class="content_tab">
 						<div class="wrap-slide">
@@ -108,112 +120,7 @@ get_header();
 						</div>
 
 						
-					</div><!-- end tab1 -->
-					<div id="Tab2" class="content_tab" style="display:none;">
-						<div class="wrap-slide">
-							<h2>Liên kế vườn - căn góc</h2>
-								<div class="pic_left">
-									<div class="over-pic">
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT3_V1__.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThugoc/BT3_V1__.RGB_color.jpg" alt="" />
-										</a>
-									</div>									
-									<div class="text-des">										
-										<h5>Liên kế góc</h5>
-										<span> 84 căn. Diện tích sử dụng là 6x12 chiếm 80% số lượng</span>
-									</div>
-									<ul style="display:none"> 
-										<li>
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT4_V1.RGB_color.jpg" title="">
-											<img src="images/Hinh_du_an/BThugoc/BT4_V1.RGB_color.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT4_V2.RGB_color.jpg" title="">
-											<img src="images/Hinh_du_an/BThugoc/BT4_V2.RGB_color.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT3A.jpg" title="">
-											<img src="images/Hinh_du_an/BThugoc/BT3A.jpg" alt="" />
-									</a></li>
-									</ul>
-								</div>
-								<div class="pic_right">
-									<div class="over-pic">
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1.RGB_color.jpg" alt="" />
-										</a>
-									</div>									
-									<div class="text-des">
-										<h5>Liên kế vườn</h5>
-										<span> 8 căn. Diện tích sử dụng là 5x12 (Biệt thự góc)</span>
-									</div>
-									<ul style="display:none"> 
-										<li>
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1A.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1A.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg" alt="" />
-										</a></li>
-									</ul>
-								</div>
-						</div>
-						
-					</div><!-- end tab2 -->
-					<div id="Tab3" class="content_tab" style="display:none;">
-						<div class="wrap-slide">
-							<h2>Mặt bằng tổng thể</h2>
-								<div class="pic_left">
-									<div class="over-pic">
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT3_V1__.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThugoc/BT3_V1__.RGB_color.jpg" alt="" />
-										</a>
-									</div>									
-									<div class="text-des">										
-										<h5>Mặt bằng tổng thể view 1</h5>
-										<span> 84 căn. Diện tích sử dụng là 6x12 chiếm 80% số lượng</span>
-									</div>
-									<ul style="display:none"> 
-										<li>
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT4_V1.RGB_color.jpg" title="">
-											<img src="images/Hinh_du_an/BThugoc/BT4_V1.RGB_color.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT4_V2.RGB_color.jpg" title="">
-											<img src="images/Hinh_du_an/BThugoc/BT4_V2.RGB_color.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThugoc/BT3A.jpg" title="">
-											<img src="images/Hinh_du_an/BThugoc/BT3A.jpg" alt="" />
-									</a></li>
-									</ul>
-								</div>
-								<div class="pic_right">
-									<div class="over-pic">
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1.RGB_color.jpg" alt="" />
-										</a>
-									</div>									
-									<div class="text-des">
-										<h5>Mặt bằng tổng thể view 2</h5>
-										<span> 8 căn. Diện tích sử dụng là 5x12 (Biệt thự góc)</span>
-									</div>
-									<ul style="display:none"> 
-										<li>
-										<a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1A.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1A.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg" alt="" />
-										</a></li>
-										<li><a class="fancybox" rel="gallery1" href="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg">
-											<img src="images/Hinh_du_an/BThuvuon/BT1_V1.RGB_color.jpg" alt="" />
-										</a></li>
-									</ul>
-								</div>
-						</div>
-						
-					</div><!-- end tab3 -->
-					
+					</div>
 
 					
 
